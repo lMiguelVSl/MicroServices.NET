6de42c4f0bdc106124e04d39bf320b8716d39cd2
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-23 22:51:35 -0500

    Sangria, Context DB and Action handler created

---
+++ Tienda_Servicios.Api.Libro/Application/Nuevo.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using MediatR;
+using Tienda_Servicios.Api.Libro.ApplicationDBContext;
+
+namespace Tienda_Servicios.Api.Libro.Application
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public string Titulo { get; set; }
+            public DateTime FechaPublicacion { get; set; }
+            public Guid? AutorLibro { get; set; }
+        }
+
+        public class EjecutaValidator : AbstractValidator<Ejecuta>
+        {
+            public EjecutaValidator()
+            {
+                RuleFor(x => x.Titulo).NotEmpty();
+                RuleFor(x => x.FechaPublicacion).NotEmpty();
+                RuleFor(x => x.AutorLibro).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Ejecuta>
+        {
+            private readonly LibreriaContexto _libreriaContexto;
+            public Handler(LibreriaContexto libreriaContexto)
+            {
+                _libreriaContexto = libreriaContexto;
+            }
+
+            public Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Tienda_Servicios.Api.Libro/ApplicationDBContext/LibreriaContexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tienda_Servicios.Api.Libro.Model;
+
+namespace Tienda_Servicios.Api.Libro.ApplicationDBContext
+{
+    public class LibreriaContexto : DbContext
+    {
+        public LibreriaContexto(DbContextOptions<LibreriaContexto> options) : base(options) {}
+
+        public DbSet<LibreriaMaterial> LibreriaMaterial { get; set; }
+    }
+}
