a6221fef6efedf93a2d9870b192b9c58395bfbed
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-23 22:50:11 -0500

    Model update

--- Tienda_Servicios.Api.Libro/Model/LibreriaMaterial.cs
+++ Tienda_Servicios.Api.Libro/Model/LibreriaMaterial.cs
@@ -3,5 +3,8 @@
     public class LibreriaMaterial
     {
         public Guid? LibreriaMaterialId { get; set; }
+        public string Titulo { get; set; }
+        public DateTime? FechaPublicacion { get; set; }
+        public Guid? AutorLibro { get; set; }
     }
 }
