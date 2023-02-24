c4d6ef28a2631cd00e25e8db30a3e0f6101fa70f
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-23 23:16:01 -0500

    Controller Done

---
+++ Tienda_Servicios.Api.Libro/Controllers/LibroMaterialController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Tienda_Servicios.Api.Libro.Application;
+
+namespace Tienda_Servicios.Api.Libro.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class LibroMaterialController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public LibroMaterialController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost ("/createBook")]
+        public async Task<ActionResult<Unit>> createBook(Nuevo.Ejecuta data)
+        {
+            return await _mediator.Send(data);
+        }
+        
+    }
+}
